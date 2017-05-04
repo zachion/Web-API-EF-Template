fc935a206355c6808643a8b60ee2eb97e1163d15
Zach Ioannou <zioannou77@gmail.com>
2017-05-04 19:26:57 +0300

    fixed parameter in model

--- CountingKs/Models/ModelFactory.cs
+++ CountingKs/Models/ModelFactory.cs
@@ -39,7 +39,7 @@ namespace CountingKs.Models
         {
             return new DiaryModel()
             {
-                Url = _urlHelper.Link("Diaries", new {diaryId = diary.CurrentDate}),
+                Url = _urlHelper.Link("Diaries", new { diaryid = diary.CurrentDate}),
                 CurrentDate = diary.CurrentDate
             };
 
