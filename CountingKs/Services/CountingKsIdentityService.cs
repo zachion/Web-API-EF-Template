b23dd88347cbbdf5cda777b524e0a124a0a9cda1
Zach Ioannou <zioannou77@gmail.com>
2017-05-04 19:27:17 +0300

    added correct hardcoded user name to actually retrieve something

--- CountingKs/Services/CountingKsIdentityService.cs
+++ CountingKs/Services/CountingKsIdentityService.cs
@@ -11,7 +11,7 @@ namespace CountingKs.Services
         {
             get
             {
-                return "ZachIoannou";
+                return "shawnwildermuth";
             }
         }
     }
