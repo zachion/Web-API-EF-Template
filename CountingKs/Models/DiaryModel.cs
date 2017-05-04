decbcf49267beb83c612dcdcb58e0bc793ee44d4
Zach Ioannou <zioannou77@gmail.com>
2017-05-04 18:47:32 +0300

    added model factory and models

---
+++ CountingKs/Models/DiaryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CountingKs.Data.Entities;
+
+namespace CountingKs.Models
+{
+    public class DiaryModel
+    {
+        public string Url {get; set; }
+        public DateTime CurrentDate { get; set; }
+        //public IEnumerable<DiaryEntryModel> Entries { get; set; }
+
+    }
+}
---
+++ CountingKs/Models/ModelFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Routing;
+using CountingKs.Data.Entities;
+
+namespace CountingKs.Models
+{
+    public class ModelFactory
+    {
+        private UrlHelper _urlHelper;
+
+        public ModelFactory(HttpRequestMessage request)
+        {
+            _urlHelper = new UrlHelper(request);
+        }
+        public FoodModel Create(Food food)
+        {
+            return new FoodModel()
+            {
+                Url = _urlHelper.Link("Food", new {foodid = food.Id}),
+                Description = food.Description,
+                Measures = food.Measures.Select(m => Create(m))
+            };
+        }
+        public MeasureModel Create(Measure measure)
+        {
+            return new MeasureModel()
+            {
+                Url = _urlHelper.Link("Measure", new { foodid = measure.Food.Id ,id=measure.Id}),
+                Description = measure.Description,
+                Calories = measure.Calories
+            };
+        }
+
+        public DiaryModel Create(Diary diary)
+        {
+            return new DiaryModel()
+            {
+                Url = _urlHelper.Link("Diaries", new {diaryId = diary.CurrentDate}),
+                CurrentDate = diary.CurrentDate
+            };
+
+        }
+    }
+}
