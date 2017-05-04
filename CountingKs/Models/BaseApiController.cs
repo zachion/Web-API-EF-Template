132f44fc9ddaa96cf33224a99594fbc61adb6a54
Zach Ioannou <zioannou77@gmail.com>
2017-05-04 18:46:40 +0300

    added BaseApiController

---
+++ CountingKs/Models/BaseApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using CountingKs.Data;
+
+namespace CountingKs.Models
+{
+    public class BaseApiController : ApiController
+    {
+        private ICountingKsRepository _repo;
+        private ModelFactory _modelFactory;
+        public BaseApiController(ICountingKsRepository repo)
+        {
+            _repo = repo;
+        }
+
+        protected ICountingKsRepository TheRepository
+        {
+            get
+            {
+                return _repo;
+            }
+        }
+
+        protected ModelFactory TheModelFactory
+        {
+            get
+            {
+                if (_modelFactory == null)
+                {
+                    _modelFactory = new ModelFactory(this.Request);
+                }
+                return _modelFactory;
+            }
+        }
+
+    }
+}
