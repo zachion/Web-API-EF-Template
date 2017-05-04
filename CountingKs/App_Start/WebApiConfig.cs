aa37505923abedb1c0627865ad78e82038314e03
Zach Ioannou <zioannou77@gmail.com>
2017-05-04 18:44:50 +0300

    added custom routes

--- CountingKs/App_Start/WebApiConfig.cs
+++ CountingKs/App_Start/WebApiConfig.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
@@ -7,24 +6,35 @@ using Newtonsoft.Json.Serialization;
 
 namespace CountingKs
 {
-  public static class WebApiConfig
-  {
-    public static void Register(HttpConfiguration config)
+    public static class WebApiConfig
     {
-      config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
-      );
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                "Food",
+                "api/nutrition/foods/{foodid}",
+                new {controller = "foods", foodid = RouteParameter.Optional} 
+            );
+            config.Routes.MapHttpRoute(
+                "Measure",
+                "api/nutrition/foods/{foodid}/measure/{id}",
+                new {controller = "measures", id = RouteParameter.Optional}
+            );
+            config.Routes.MapHttpRoute(
+                "Diaries",
+                "api/user/diaries/{diaryid}",
+                new { controller = "diaries", diaryid = RouteParameter.Optional }
+            );
 
-      // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
-      // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
-      // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
-      //config.EnableQuerySupport();
+            // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
+            // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
+            // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
+            //config.EnableQuerySupport();
 
-        var jsonFormater = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
-        if (jsonFormater != null)
-            jsonFormater.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //allow camel casing in the response objects
+            var jsonFormater = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
+            if (jsonFormater != null)
+                jsonFormater.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
     }
-  }
 }
