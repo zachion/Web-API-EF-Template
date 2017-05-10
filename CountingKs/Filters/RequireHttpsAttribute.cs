926fc1994b6752bbb3e1276dddde7b14aaac82ec
Zach Ioannou <zioannou77@gmail.com>
2017-05-10 14:44:33 +0300

    added RwequireHttps attribute

---
+++ CountingKs/Filters/RequireHttpsAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CountingKs.Filters
+{
+    public class RequireHttpsAttribute : AuthorizationFilterAttribute
+    {
+        [RequireHttps]
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            var req = actionContext.Request;
+
+            if (req.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                var html = "<p>http is required</p>";
+                if (req.Method.Method == "GET")
+                {
+                    actionContext.Response = req.CreateResponse(HttpStatusCode.Found);
+                    actionContext.Response.Content = new StringContent(html, Encoding.UTF8, "text/html");
+                    var uriBuilder = new UriBuilder(req.RequestUri);
+                    uriBuilder.Scheme = Uri.UriSchemeHttps;
+                    uriBuilder.Port = 443;
+
+                    actionContext.Response.Headers.Location = uriBuilder.Uri;
+                }
+                else
+                {
+                    actionContext.Response = req.CreateResponse(HttpStatusCode.NotFound);
+                    actionContext.Response.Content = new StringContent(html, Encoding.UTF8, "text/html");
+                }
+
+            }
+        }
+
+    }
+}
