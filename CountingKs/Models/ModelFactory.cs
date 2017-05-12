ab3465e6d80e35a52cdca9513337f54122e42c0d
Zach Ioannou <zioannou77@gmail.com>
2017-05-12 15:04:44 +0300

    added MeasureV2Model in modelfactory

--- CountingKs/Models/ModelFactory.cs
+++ CountingKs/Models/ModelFactory.cs
@@ -28,6 +28,26 @@ namespace CountingKs.Models
                 Measures = food.Measures.Select(m => Create(m))
             };
         }
+
+        public MeasureV2Model Create2(Measure measure)
+        {
+            return new MeasureV2Model()
+            {
+                Url = _urlHelper.Link("Measures", new { foodid = measure.Food.Id, id = measure.Id }),
+                Description = measure.Description,
+                Calories = measure.Calories,
+                Carbohydrates = measure.Carbohydrates,
+                Cholestrol = measure.Cholestrol,
+                Fiber = measure.Fiber,
+                Iron = measure.Iron,
+                Protein = measure.Protein,
+                SaturatedFat = measure.SaturatedFat,
+                Sodium = measure.Sodium,
+                Sugar = measure.Sugar,
+                TotalFat = measure.TotalFat
+            };
+        }
+
         public MeasureModel Create(Measure measure)
         {
             return new MeasureModel()
